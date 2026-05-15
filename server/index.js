688b46dedde4f83734d4b5eb3ba3772bc35dcf3c
Ray <rcastro2025@alu.uct.cl>
2026-05-15 15:02:02 -0400

    feat: pagina

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("¡Conectado exitosamente a MongoDB!"))
   .catch((err) => console.error("Error al conectar a MongoDB:", err));
 
-// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('Servidor MERN funcionando correctamente');
 });
