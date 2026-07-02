3efecfb7f189ea900baffae69ce3d4f4862818cb
Ray <rcastro2025@alu.uct.cl>
2026-07-02 13:55:43 -0400

    test: test E2E

---
+++ client/cypress/e2e/login.cy.js
@@ -0,0 +1,19 @@
+// client/cypress/e2e/login.cy.js
+describe("Flujo de Autenticación SGAU", () => {
+  it("Debe permitir a un usuario iniciar sesión correctamente", () => {
+    // 1. El robot visita tu página (asegúrate de tener tu localhost corriendo)
+    cy.visit("http://localhost:5173");
+
+    // 2. El robot busca el campo de correo y escribe
+    cy.get('input[type="email"]').type("admin@uct.cl");
+
+    // 3. El robot busca el campo de contraseña y escribe
+    cy.get('input[type="password"]').type("admin1234");
+
+    // 4. El robot hace clic en el botón de entrar
+    cy.contains("button", "Ingresar al Sistema").click();
+
+    // 5. Verificamos que el login fue exitoso buscando algo de la siguiente pantalla
+    cy.contains("Sistema Académico Integrado").should("be.visible");
+  });
+});
